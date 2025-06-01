6c3037f3af2a159d2f0cc1bdbd9d184c17719dc4
diagne <diagnebuyengo@gmail.com>
2025-06-01 08:03:00 +0300

    made a date and time object to track tasks

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,4 +1,9 @@
+
 class Todo:
     def __init__(self, task, category,
                  date_added=None, date_completed=None,
                  status=None, position=None):
+        self.task = task
+        self.category = category
+        self.date_added = date_added if date_added is not None else datetime.datetime.now().isoformat()
+
